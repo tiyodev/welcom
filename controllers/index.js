3b5915bc6927570f128420c2b435b9017e3df099
Tiyo <tiyodev@gmail.com>
2018-04-24 11:19:46 +0200

    redo experience card

--- controllers/index.js
+++ controllers/index.js
@@ -7,22 +7,25 @@ exports.getIndex = (req, res) => {
     headerPic: '/images/default-img-card.jpg',
     nbRecommendation: 5,
     profilePic: '/images/default-profile-pic.jpg',
+    displayName: 'Thomasson-Thomasson-Thomasson',
+    expDesc: 'Explore the wonderful Golden Triangle together Explore the wonderful Golden',
+    isFree: true
+  },
+  {
+    headerPic: '/images/default-img-card.jpg',
+    nbRecommendation: 0,
+    profilePic: '/images/default-profile-pic.jpg',
     displayName: 'Thomasson',
-    expDesc: 'Explore the wonderful Golden Triangle together'
+    expDesc: 'Explore the wonderful Golden Triangle together',
+    isFree: false
   },
   {
     headerPic: '/images/default-img-card.jpg',
     nbRecommendation: 5,
     profilePic: '/images/default-profile-pic.jpg',
     displayName: 'Thomasson',
-    expDesc: 'Explore the wonderful Golden Triangle together'
-  },
-  {
-    headerPic: '/images/default-img-card.jpg',
-    nbRecommendation: 5,
-    profilePic: '/images/default-profile-pic.jpg',
-    displayName: 'Thomasson',
-    expDesc: 'Explore the wonderful Golden Triangle together'
+    expDesc: 'Explore the wonderful Golden Triangle together',
+    isFree: false
   }];
 
   res.render('index', { title: 'Welcom\' Home', exps });
